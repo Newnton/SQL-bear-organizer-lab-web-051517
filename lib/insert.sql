6703e56261e43f5a42858625f88c14ac8699def8
Zach Dubno <zach@Zach-Dubnos-MacBook-Pro.local>
2017-05-22 13:04:11 -0400

    finished lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 12, 'M', "Brown", "Passive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 8, 'M', "Black", "Aggresive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 7, 'F', "Brown", "Aggressive", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 14, 'M', "Brown", "Passive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 22, 'F', "Black", "Passive", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 34, 'M', "Brown", "Passive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 2, 'F', "Brown", "Passive", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 12, 'M', "Black", "Aggressive", 1);
